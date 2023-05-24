9c82d011cd3e73b4eb1d1f4686088110fd456d70
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-05-24 15:19:52 -0400

    infinite scrolling working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import useBookSearch from "./useBookSearch";
 
 function App() {
   const [query, setQuery] = useState('')
   const [pageNumber, setPageNumber] = useState(1)
-  useBookSearch(query, pageNumber)
+
+  const {
+    books,
+    hasMore,
+    loading,
+    error
+     } = useBookSearch(query, pageNumber)
+
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return 
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if(entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if (node) observer.current.observe(node)
+    console.log(node)
+  }, [loading, hasMore])
 
   function handleSearch(e) {
     setQuery(e.target.value)
@@ -12,14 +31,16 @@ function App() {
   }
   return (
     <>
-    <input type="text" onChange={handleSearch}></input>
-    <div>Title</div>
-    <div>Title</div>
-    <div>Title</div>
-    <div>Title</div>
-    <div>Title</div>
-    <div>Loading...</div>
-    <div>Error</div>
+    <input type="text" value={query}  onChange={handleSearch}></input>
+    {books.map((book, index) => {
+      if (books.length === index + 1) {
+        return <div ref={lastBookElementRef} key={book}>{book}</div>
+      } else {
+        return <div key={book}>{book}</div>
+}
+    })}
+    <div>{loading && 'Loading...'}</div>
+    <div>{error && 'Error'}</div>
     </>
   )
 }
